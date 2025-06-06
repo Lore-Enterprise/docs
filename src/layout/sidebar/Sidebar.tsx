04992f2ac526ed619d8bb8fe9eb8cb0ee15ef2a1
mmorrii <byryshka55@gmail.com>
2025-06-07 01:32:34 +0300

    add styles for sidebar

--- src/layout/sidebar/Sidebar.tsx
+++ src/layout/sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { SidebarItem } from "../../types/sidebar.ts"
 import { NavLink } from "react-router"
 import styles from "./Sidebar.module.css"
+import clsx from "clsx"
 
 export default function Sidebar() {
   const [sidebarItems, setSidebarItems] = useState<SidebarItem[]>([])
@@ -21,7 +22,10 @@ export default function Sidebar() {
               {renderItems(item.children)}
             </>
           ) : (
-            <NavLink to={{ pathname: item.path === "/index" ? "/" : item.path }} className={styles.link}>
+            <NavLink
+              to={{ pathname: item.path === "/index" ? "/" : item.path }}
+              className={({ isActive }) => clsx(styles.link, isActive && styles.active)}
+            >
               {item.title}
             </NavLink>
           )}
@@ -32,8 +36,8 @@ export default function Sidebar() {
 
   return (
     <aside>
-      <nav>
-        { renderItems(sidebarItems) }
+      <nav className={styles.nav}>
+        {renderItems(sidebarItems)}
       </nav>
     </aside>
   )
